c4b5208fe95e0d6fcf80abb83a7faf8658089061
D1elit <dropdox12@gmail.com>
2025-08-06 19:15:01 +0200

    init

---
+++ src/db/in-memory.db.ts
@@ -0,0 +1,36 @@
+import {Video, availableResolutions} from "../videos/types/video";
+
+export const db = {
+  video: <Video[]>[
+    {
+      id: 1,
+      title: 'How to learn js',
+      author: 'Marko Polo',
+      canBeDownloaded: false,
+      minAgeRestriction: 18,
+      createdAt: new Date(),
+      publicationDate: new Date(),
+      availableResolutions: Object.values(availableResolutions),
+    },
+    {
+      id: 2,
+      title: 'How to learn Express',
+      author: 'James Blunt',
+      canBeDownloaded: false,
+      minAgeRestriction: 18,
+      createdAt: new Date(),
+      publicationDate: new Date(),
+      availableResolutions: Object.values(availableResolutions),
+    },
+    {
+      id: 3,
+      title: 'How to learn MongoDB',
+      author: 'Rick Rubin',
+      canBeDownloaded: false,
+      minAgeRestriction: 18,
+      createdAt: new Date(),
+      publicationDate: new Date(),
+      availableResolutions: Object.values(availableResolutions),
+    },
+  ],
+};
